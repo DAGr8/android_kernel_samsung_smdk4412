52a495923fb85b4847f392b57ae0e0a0d3fcd171
Andrei F <luxneb@gmail.com>
2012-12-17 23:29:54 +0100

    mali: Add control for utilization timeout
    Conflicts:
    
    	drivers/media/video/samsung/mali/common/mali_kernel_utilization.c
    	drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform_dvfs.c

--- drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform_dvfs.c
+++ drivers/media/video/samsung/mali/platform/pegasus-m400/mali_platform_dvfs.c
@@ -458,6 +458,7 @@ static unsigned int decideNextStatus(unsigned int utilization)
 	if (mali_runtime_resumed >= 0) {
 		level = mali_runtime_resumed;
 		mali_runtime_resumed = -1;
+		return level;
 	}
 
 	if (mali_dvfs_threshold[maliDvfsStatus.currentStep].upthreshold
@@ -924,6 +925,32 @@ static ssize_t thresholds_store(struct sysdev_class * cls, struct sysdev_class_a
 	return count;	
 }
 
+static ssize_t utilization_timeout_show(struct sysdev_class * cls, 
+			     struct sysdev_class_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d", mali_gpu_utilization_timeout);
+}
+
+static ssize_t utilization_timeout_store(struct sysdev_class * cls, struct sysdev_class_attribute *attr,
+			      const char *buf, size_t count) 
+{
+	unsigned int ret = -EINVAL;
+	int ms;
+
+	ret = sscanf(buf, "%d", &ms);
+	if (ret != 1) {
+		return -EINVAL;
+	} else {
+		if(ms < 25)
+			ms = 25;
+		if(ms > 2000)
+			ms = 2000;
+
+		mali_gpu_utilization_timeout = ms;
+	}
+	return count;	
+}
+
 static ssize_t current_freq_show(struct sysdev_class * cls, 
 			     struct sysdev_class_attribute *attr, char *buf)
 {
@@ -971,6 +998,7 @@ static SYSDEV_CLASS_ATTR(min_freq, S_IRUGO | S_IWUGO, min_freq_show, min_freq_st
 static SYSDEV_CLASS_ATTR(freq_table, S_IRUGO | S_IWUGO, freq_table_show, freq_table_store);
 static SYSDEV_CLASS_ATTR(volt_table, S_IRUGO | S_IWUGO, volt_table_show, volt_table_store);
 static SYSDEV_CLASS_ATTR(thresholds, S_IRUGO | S_IWUGO, thresholds_show, thresholds_store);
+static SYSDEV_CLASS_ATTR(utilization_timeout, S_IRUGO | S_IWUGO, utilization_timeout_show, utilization_timeout_store);
 static SYSDEV_CLASS_ATTR(current_freq, S_IRUGO, current_freq_show, NULL);
 static SYSDEV_CLASS_ATTR(time_in_state, S_IRUGO | S_IWUGO, time_in_state_show, time_in_state_store);
 
@@ -980,6 +1008,7 @@ static struct sysdev_class_attribute *gpu_attributes[] = {
 	&attr_freq_table,
 	&attr_volt_table,
 	&attr_thresholds,
+	&attr_utilization_timeout,
 	&attr_current_freq,
 	&attr_time_in_state,
 };
